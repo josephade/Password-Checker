ef94b9d54e01a03eea60fd426eba02fd5e6b6d94
josephade <56506354+josephade@users.noreply.github.com>
2020-12-04 23:54:06 +0000

    Update passwordchecker.py

--- passwordchecker.py
+++ passwordchecker.py
@@ -6,7 +6,7 @@ def request_api_data(query_char):
   url = 'https://api.pwnedpasswords.com/range/' + query_char
   res = requests.get(url)
   if res.status_code != 200:
-    raise RuntimeError(f'Error fetching: {res.status_code}, check the api and try again')
+    raise RuntimeError('Error fetching: {}, check the api and try again'.format(res.status_code))
   return res
 
 def get_password_leaks_count(hashes, hash_to_check):
@@ -17,6 +17,7 @@ def get_password_leaks_count(hashes, hash_to_check):
   return 0
 
 def pwned_api_check(password):
+  #check password if it exist in API response 
   sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
   first5_char, tail = sha1password[:5], sha1password[5:]
   response = request_api_data(first5_char)
@@ -26,9 +27,9 @@ def main(args):
   for password in args:
     count = pwned_api_check(password)
     if count:
-      print(f'{password} was found {count} times... you should probably change your password!')
+      print('{} was found {} times... you should probably change your password!'.format(password, count))
     else:
-      print(f'{password} was NOT found. Carry on!')
+      print('{} was NOT found. Carry on!'.format(password))
   return 'done!'
 
 if __name__ == '__main__':
